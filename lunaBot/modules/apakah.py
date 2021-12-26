614d51ec42f3cd0449b09e3565ca8000561e3d4d
zeinzo <91390078+zeinzo@users.noreply.github.com>
2021-12-26 13:04:45 +0700

    Create apakah.py

---
+++ lunaBot/modules/apakah.py
@@ -0,0 +1,34 @@
+import random
+import requests
+from lunaBot.events import register
+from lunaBot import telethn
+from bs4 import BeautifulSoup as bs
+from telethon.errors import ChatSendMediaForbiddenError
+from telegram import (
+    Update,
+    ParseMode,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+)
+from telegram.ext import CallbackContext
+APAKAH_STRING = ["Iya", 
+                 "Tidak", 
+                 "Mungkin", 
+                 "Mungkin Tidak", 
+                 "Bisa jadi", 
+                 "Mungkin Tidak",
+                 "Tidak Mungkin",
+                 "YNTKTS",
+                 "Pala bapak kau pecah",
+                 "Apa iya?",
+                 "Tanya aja sama mamak kau tu pler"
+                 ]
+
+
+@register(pattern="^/apakah ?(.*)")
+async def apakah(event):
+    quew = event.pattern_match.group(1)
+    if not quew:
+        await event.reply('Berikan saya pertanyaan 😐')
+        return
+    await event.reply(random.choice(APAKAH_STRING))
