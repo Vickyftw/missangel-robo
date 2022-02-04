9c5a2acaae4d8821c7f8621a150335f1d32c0692
||√HYPER^£•°||π <92142624+HYPER-AD17@users.noreply.github.com>
2022-02-04 19:41:04 +0530

    alive

--- lunaBot/modules/alive.py
+++ lunaBot/modules/alive.py
@@ -7,23 +7,23 @@ from pyrogram import __version__ as pyrover
 from lunaBot.events import register as MEMEK
 from lunaBot import telethn as tbot
 
-PHOTO = "https://telegra.ph/file/7c3c26e0ed938aec91209.jpg"
+ALIVE_LOGO = "https://telegra.ph/file/f7b0387db2dd5070e6eab.mp4"
 
-@MEMEK(pattern=("/alive"))
+@MEMEK(pattern=("/alibe"))
 async def awake(event):
   tai = event.sender.first_name
-  LUNA = "**Holla I'm Luna!** \n\n"
-  LUNA += "🔴 **I'm Working Properly** \n\n"
-  LUNA += "🔴 **My Master : [zeinzo](https://t.me/tdrki_1)** \n\n"
-  LUNA += f"🔴 **Telethon Version : {tlhver}** \n\n"
-  LUNA += f"🔴 **Pyrogram Version : {pyrover}** \n\n"
-  LUNA += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/lunatapibot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/lunaXresso")]]
-  await tbot.send_file(event.chat_id, PHOTO, caption=LUNA,  buttons=BUTTON)
+  DEVU = "**HoII!, I'm Devu Robot!** \n\n"
+  DEVU += "🔴 **I'm Working Properly** \n\n"
+  DEVU += "🔴 **My Master : [HYPER](https://t.me/HYPER_AD17)** \n\n"
+  DEVU += f"🔴 **Telethon Version : {tlhver}** \n\n"
+  DEVU += f"🔴 **Pyrogram Version : {pyrover}** \n\n"
+  DEVU += "**Thanks For Adding Me Here ❤️**"
+  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/DEVU_ROBOT?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/SCILENT_BOTS")]]
+  await tbot.send_file(event.chat_id, ALIVE_LOGO, caption=LUNA,  buttons=BUTTON)
 
 @MEMEK(pattern=("/reload"))
 async def reload(event):
   tai = event.sender.first_name
   LUNA = "✅ **bot restarted successfully**\n\n• Admin list has been **updated**"
-  BUTTON = [[Button.url("📡 ᴜᴘᴅᴀᴛᴇs", "https://t.me/zeinproject")]]
-  await tbot.send_file(event.chat_id, PHOTO, caption=LUNA,  buttons=BUTTON)
+  BUTTON = [[Button.url("📡 ᴜᴘᴅᴀᴛᴇs", "https://t.me/SCILENT_BOTS")]]
+  await tbot.send_file(event.chat_id, ALIVE_LOGO, caption=LUNA,  buttons=BUTTON)
