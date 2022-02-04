7254d9229c16dabd0395d6898ee29c53e2d8b1df
||√HYPER^£•°||π <92142624+HYPER-AD17@users.noreply.github.com>
2022-02-04 19:46:11 +0530

    ZZ

--- lunaBot/modules/alive.py
+++ lunaBot/modules/alive.py
@@ -7,7 +7,8 @@ from pyrogram import __version__ as pyrover
 from lunaBot.events import register as MEMEK
 from lunaBot import telethn as tbot
 
-ALIVE_LOGO = "https://telegra.ph/file/f7b0387db2dd5070e6eab.mp4"
+RELOAD_LOGO = "https://telegra.ph/file/f7b0387db2dd5070e6eab.mp4"
+ALIBE_LOGO = "https://telegra.ph/file/179825db677596b9f574d.mp4"
 
 @MEMEK(pattern=("/alibe"))
 async def awake(event):
@@ -19,11 +20,11 @@ async def awake(event):
   DEVU += f"🔴 **Pyrogram Version : {pyrover}** \n\n"
   DEVU += "**Thanks For Adding Me Here ❤️**"
   BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/DEVU_ROBOT?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/SCILENT_BOTS")]]
-  await tbot.send_file(event.chat_id, ALIVE_LOGO, caption=LUNA,  buttons=BUTTON)
+  await tbot.send_file(event.chat_id, ALIBE_LOGO, caption=DEVU,  buttons=BUTTON)
 
 @MEMEK(pattern=("/reload"))
 async def reload(event):
   tai = event.sender.first_name
   LUNA = "✅ **bot restarted successfully**\n\n• Admin list has been **updated**"
   BUTTON = [[Button.url("📡 ᴜᴘᴅᴀᴛᴇs", "https://t.me/SCILENT_BOTS")]]
-  await tbot.send_file(event.chat_id, ALIVE_LOGO, caption=LUNA,  buttons=BUTTON)
+  await tbot.send_file(event.chat_id, RELOAD_LOGO, caption=DEVU,  buttons=BUTTON)
