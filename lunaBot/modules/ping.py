42c3ff64ab09777d9774bf1c54e911d2dd8da0e5
||√HYPER^£•°||π <92142624+HYPER-AD17@users.noreply.github.com>
2022-02-04 20:00:32 +0530

    Update ping.py

--- lunaBot/modules/ping.py
+++ lunaBot/modules/ping.py
@@ -74,7 +74,7 @@ def ping(update: Update, context: CallbackContext):
     msg = update.effective_message
 
     start_time = time.time()
-    message = msg.reply_text("Pinging...")
+    message = msg.reply_text("🥲")
     end_time = time.time()
     telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
     uptime = get_readable_time((time.time() - StartTime))
