356b5e58c64444e1cda91aaaaa3b23d34ef00112
||√HYPER^£•°||π <92142624+HYPER-AD17@users.noreply.github.com>
2022-02-05 19:30:40 +0530

    Update __main__.py

--- lunaBot/__main__.py
+++ lunaBot/__main__.py
@@ -75,7 +75,7 @@ def get_readable_time(seconds: int) -> str:
 START_LOGO = "https://telegra.ph/file/736c87fcd90273108348c.mp4"
 
 PM_START_TEXT = (f'''
-𝗛𝗲𝘆! , 𝗜 𝗮𝗺 ᴅᴇᴠᴜ ʀᴏʙᴏᴛ「⚜️」[😎](https://telegra.ph/file/f31af856c345e00513c36.mp4)
+𝗛𝗲𝘆! {message.from_user.mention()}, 𝗜 𝗮𝗺 ᴅᴇᴠᴜ ʀᴏʙᴏᴛ「⚜️」[😎](https://telegra.ph/file/f31af856c345e00513c36.mp4)
 ️◁───ꔸꔸꔸꔸꔸꔸꔸꔸꔸꔸ❚❚ꔸꔸꔸꔸꔸꔸꔸꔸꔸꔸ───▷
 `𝗜'𝗺 𝗩𝗲𝗿𝘆 𝗽𝗼𝘄𝗲𝗿𝗳𝘂𝗹𝗹 𝗠𝗮𝗻𝗻𝗮𝗴𝗲𝗺𝗲𝗻𝘁 && 𝗧𝗿𝘂𝘀𝘁𝗲𝗱 𝗥𝗼𝗯𝗼𝘁 𝗪𝗶𝘁𝗵 𝗼𝘀𝗺 𝗺𝗼𝗱𝘂𝗹𝗲𝘀.♻️`
 ️●───────❚❚───────●
