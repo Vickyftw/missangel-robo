07550323dbb0bba2b62700acf1af5fb43e5c5d12
||√HYPER^£•°||π <92142624+HYPER-AD17@users.noreply.github.com>
2022-02-11 09:27:18 +0530

    Update __main__.py

--- lunaBot/__main__.py
+++ lunaBot/__main__.py
@@ -431,10 +431,10 @@ def luna_about_callback(update, context):
                 [
                  [
                   InlineKeyboardButton(text="💝ʙᴀsɪᴄ ᴄᴍᴅs💝", callback_data="luna_admin"),
-                  InlineKeyboardButton(text="💘sᴜᴅᴏs ᴄᴍᴅ🧑‍💻", callback_data="luna_credit"),
+                  InlineKeyboardButton(text="🔯ᴀᴅᴍɪɴ ᴄᴍᴅs🔯", callback_data="luna_support"),
                  ],
                  [
-                  InlineKeyboardButton(text="🔯ᴀᴅᴍɪɴ ᴄᴍᴅs🔯", callback_data="luna_support"),
+                  InlineKeyboardButton(text="💘sᴜᴅᴏs ᴄᴍᴅ🧑‍💻", callback_data="luna_credit"),
                   InlineKeyboardButton(text="🔥ᴏᴡɴᴇʀ ᴄᴍᴅs🤍", callback_data="luna_aselole"),
                  ],
                  [
