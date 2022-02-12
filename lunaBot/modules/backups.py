8b8a9b751fe28c97c71cac1c4cec86f59872045b
||√HYPER^£•°||π <92142624+HYPER-AD17@users.noreply.github.com>
2022-02-12 09:18:05 +0530

    Update backups.py

--- lunaBot/modules/backups.py
+++ lunaBot/modules/backups.py
@@ -367,7 +367,7 @@ def get_chat(chat_id, chat_data):
         return {"status": False, "value": False}
 
 
-__mod_name__ = "Backup"
+__mod_name__ = "💾ʙᴀᴄᴋᴜᴘ💾"
 
 __help__ = """
 *Only for group owner:*
