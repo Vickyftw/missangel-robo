684b7f97bd62fdbd45b27b540fc66eb17405b836
Vickyftw <90129845+Vickyftw@users.noreply.github.com>
2022-03-12 14:57:24 +0530

    Update anime.py

--- lunaBot/modules/anime.py
+++ lunaBot/modules/anime.py
@@ -593,7 +593,7 @@ dispatcher.add_handler(KAIZOKU_SEARCH_HANDLER)
 dispatcher.add_handler(KAYO_SEARCH_HANDLER)
 dispatcher.add_handler(UPCOMING_HANDLER)
 
-__mod_name__ = "🙋‍♂️ᴀɴɪᴍᴇ🙋‍♂️"
+__mod_name__ = "ᴀɴɪᴍᴇ"
 __command_list__ = [
     "anime", "manga", "character", "user", "upcoming", "kaizoku", "airing",
     "kayo"
