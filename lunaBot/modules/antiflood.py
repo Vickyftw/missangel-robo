0618e5cf4c5a9f3da50ec774902e51a4562dc94c
Vickyftw <90129845+Vickyftw@users.noreply.github.com>
2022-03-12 15:15:58 +0530

    Update antiflood.py

--- lunaBot/modules/antiflood.py
+++ lunaBot/modules/antiflood.py
@@ -425,7 +425,7 @@ will result in restricting that user.
  `1w` = 1 week
  """
 
-__mod_name__ = "🚨ᴀɴᴛɪꜰʟᴏᴏᴅ🚨"
+__mod_name__ = "ᴀɴᴛɪ ꜰʟᴏᴏᴅ"
 
 FLOOD_BAN_HANDLER = MessageHandler(
     Filters.all & ~Filters.status_update & Filters.group, check_flood
