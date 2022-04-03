7cbcb02045c8230409e1d2d196ea34909984f8aa
Vickyftw <90129845+Vickyftw@users.noreply.github.com>
2022-04-03 10:34:25 +0530

    Update userinfo.py

--- lunaBot/modules/userinfo.py
+++ lunaBot/modules/userinfo.py
@@ -422,7 +422,7 @@ def set_about_me(update: Update, context: CallbackContext):
 @run_async
 @sudo_plus
 def stats(update: Update, context: CallbackContext):
-    stats = "<b>┎─⌈ ᴄᴜʀʀᴇɴᴛ sᴛᴀᴛs ᴏғ Miss Angel ⌋</b>\n" + "\n".join([mod.__stats__() for mod in STATS])
+    stats = "<b>┎─⌈ ᴄᴜʀʀᴇɴᴛ sᴛᴀᴛs ᴏғ ᴍɪꜱꜱ ᴀɴɢᴇʟ ⌋</b>\n" + "\n".join([mod.__stats__() for mod in STATS])
     result = re.sub(r"(\d+)", r"<code>\1</code>", stats)
     update.effective_message.reply_text(result, parse_mode=ParseMode.HTML)
 
